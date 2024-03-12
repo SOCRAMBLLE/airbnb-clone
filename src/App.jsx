e122fcaf7b427c75d5db41aa34226438c8e9f2ac
Marcos André Martins Fraga <mmartins@lab4tech.lan>
2024-03-12 11:05:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return <Navbar />;
+}
